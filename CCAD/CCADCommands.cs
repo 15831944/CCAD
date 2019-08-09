76eca0aeef2c0d17b89daad826f0cb988118f13b
cc <albertcheng126@126.com>
2019-08-10 01:58:16 +0800

    command:add new model command INSERTBLOCKTOPOINT

---
+++ CCAD/CCADCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using CCAD;
+using CCAD.Commands;
+
+[assembly: CommandClass(typeof(CCAD.CCADCommands))]
+
+
+namespace CCAD
+{
+    public class CCADCommands 
+    {
+        [CommandMethod("CCAD", "INSERTBLOCKTOPOINT", CommandFlags.Modal)]
+        public void InsertBlockToPoint()
+        {
+            InsertBlockToPointCommand cmd = new InsertBlockToPointCommand();
+            cmd.Execute();
+        }
+    }
+}
